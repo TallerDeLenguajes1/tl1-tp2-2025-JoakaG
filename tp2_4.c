e30f14a1a33c55b852fe6cac3a33c1c9e79a344a
Joaquina Girbau <girbaujoaquina2@gmail.com>
2025-04-01 13:43:08 -0300

    funcion listarPCs terminada

--- tp2_4.c
+++ tp2_4.c
@@ -10,7 +10,7 @@ struct compu {
  char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
 }typedef Compu;
 
-
+void listarPCs(Compu compus[], int x);
 
 int main(){
 srand(time(NULL));
@@ -23,13 +23,18 @@ Compu compus[] = {
     {rand() % 3 + 1, rand() % 10 + 2015, rand() % 8 + 1, tipos[rand() % 6]}
 };
 
-    for (int i = 0; i < 5; i++)
-    {
-    printf("---------Compu N%d--------\n Velocidad: %d\n Anio: %d\n Nucleos: %d\n Modelo CPU: %s\n", i+1, compus[i].velocidad, compus[i].anio, compus[i].cantidad_nucleos, compus[i].tipo_cpu);
-        
-    }
+listarPCs(compus, 5);
+
+   
     
 
 
     return 0;
+}
+
+void listarPCs(Compu compus[], int x){
+     for (int i = 0; i < 5; i++)
+    {
+    printf("---------Compu N%d--------\n Velocidad: %d\n Anio: %d\n Nucleos: %d\n Modelo CPU: %s\n", i+1, compus[i].velocidad, compus[i].anio, compus[i].cantidad_nucleos, compus[i].tipo_cpu);
+    }
 }
